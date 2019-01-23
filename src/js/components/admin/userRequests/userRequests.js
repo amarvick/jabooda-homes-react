2a3366f5c9fe0c68af7d91886df8ba28761ada99
Alex <amarvick94@gmail.com>
2019-01-23 14:48:22 -0800

    Working on pending users on Admin page

--- src/js/components/admin/userRequests/userRequests.js
+++ src/js/components/admin/userRequests/userRequests.js
@@ -6,50 +6,121 @@ import { connect } from 'react-redux'
 
 import '../../../../stylesheets/userRequests.scss'
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
+import Modal from '@material-ui/core/Modal'
 
 import LoadingScreen from '../../loadingScreen'
 import ErrorScreen from '../../errorScreen'
+import UserRequestsModal from './userRequestsModal'
 
-// import { loadData } from '../../actions/careerActions'
+import { loadData } from '../../../actions/userActions'
+
+function getModalStyle() {
+    const top = 50
+    const left = 50
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const styles = theme => ({
+    paper: {
+        position: 'absolute',
+        width: theme.spacing.unit * 50,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing.unit * 4,
+    },
+});
 
 class UserRequests extends Component {
+    constructor() {
+        this.state = {
+            displayPendingUsersModal: false
+        }
+    }
 
-  // Displaying all pending user requests
-  render(props) {
-    // var pendingRequestCount = this.props.requestData.length || 0
-    var areOrIs, singularOrPlural
-   
-    // if (requestData !== 1) {
-    //     areOrIs = 'is'
-    //     singularOrPlural = ''
-    // } else {
-    //     areOrIs = 'are'
-    //     singularOrPlural = 's'
-    // }
+    componentWillMount() {
+        this.props.dispatch(loadData())
+    }
 
-    return (
-      <div>
-        <p>
-            eeeeeeeeee
-            {/* There { areOrIs } { pendingRequestCount } new user request{singularOrPlural}. */}
-        </p>
-      </div>
-    )
-  }
+    showPendingUsers() {
+        this.setState({ displayPendingUsersModal: true })
+    }
+
+    closePendingUsers() {
+        this.setState({ displayPendingUsersModal: false })
+    }
+
+    // Displaying all pending user requests
+    render(props) {
+        const { classes } = this.props;
+        var pendingRequestCount = 0
+        var showPendingUsers
+        var areOrIs, singularOrPlural
+
+        for (var i = 0; i < this.props.userData.length; i++) {
+            if (this.props.userData[i].pending === true) {
+                pendingRequestCount++
+            }
+        }
+
+        if (pendingRequestCount === 1) {
+            areOrIs = 'is'
+            singularOrPlural = ''
+        } else {
+            areOrIs = 'are'
+            singularOrPlural = 's'
+        }
+
+        if (pendingRequestCount >= 0) {
+            showPendingUsers = (
+                <Button onClick={this.showPendingUsers()}>
+                    Show all pending users
+                </Button>
+            )
+        }
+
+        return (
+            <div>
+                <p>
+                    There {areOrIs} {pendingRequestCount} new user request{singularOrPlural}.
+            {showPendingUsers}
+                </p>
+
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={this.state.displayPendingUsersModal}
+                    onClose={this.closePendingUsers}
+                    disableBackdropClick={true}
+                >
+                    <UserRequestsModal
+                        style={getModalStyle()}
+                        className={classes.paper}
+                        jobTitle={this.props.header}
+                        closePendingUsers={this.closePendingUsers} />
+                </Modal>
+            </div>
+        )
+    }
 }
 // wraps dispatch to create nicer functions to call within our component
 // Mapping dispatch actions to the props
 const mapDispatchToProps = (dispatch) => ({
-  dispatch: dispatch,
-  startup: () => dispatch(StartupActions.startup())
+    dispatch: dispatch,
+    startup: () => dispatch(StartupActions.startup())
 })
 
 // Maps the state in to props (for displaying on the front end)
 const mapStateToProps = (state) => ({
-  state: state,
-  error: state.career.error,
-  loading: state.career.loading,
-  careerData: state.career.careerData
+    state: state,
+    error: state.user.error,
+    loading: state.user.loading,
+    userData: state.user.userData
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserRequests);
