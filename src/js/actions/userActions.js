45699a048aab5b164ea78b1575b603c026e246d5
Alex <amarvick94@gmail.com>
2019-01-31 08:36:14 -0500

    Reworking user actions, may make joint actions file for CRUD operations

--- src/js/actions/userActions.js
+++ src/js/actions/userActions.js
@@ -32,35 +32,38 @@ export function loadUserData() {
     }
 }
 
+// AM - just forward this to 'editUser' since that is what you are doing essentially? Make naming conventions good
 export function approveUser(theUser) {
     console.log(theUser)
     return async function action(dispatch) {
-        const updateNewUser = await axios.post('http://localhost:3001/api/updateUserData', theUser)
+        // const updateNewUser = await axios.post('http://localhost:3001/api/updateUserData', theUser)
 
-        .then(function(response) {
-            console.log(response)
-            // Redirect user to home page notifying them that the user has been approved
-        })
+        // .then(function(response) {
+        //     console.log(response)
+        //     // Redirect user to home page notifying them that the user has been approved
+        // })
 
-        .catch(function(error) {
-            console.log(error)
-        })
+        // .catch(function(error) {
+        //     console.log(error)
+        // })
+        dispatch(editUser(theUser))
     }
 }
 
+// AM - forward this to deleteUser function. Keep this for naming convention sake
 export function rejectUser(theUser) {
     return async function action(dispatch) {
+        // const deleteNewUser = await axios.post('/api/deleteUserData', theUser)
 
-        const deleteNewUser = await axios.post('/api/deleteUserData', theUser)
+        // .then(function(response) {
+        //     console.log(response)
+        //     // Redirect user to home page notifying them that the user has been deleted
+        // })
 
-        .then(function(response) {
-            console.log(response)
-            // Redirect user to home page notifying them that the user has been deleted
-        })
-
-        .catch(function(error) {
-            console.log(error)
-        })
+        // .catch(function(error) {
+        //     console.log(error)
+        // })
+        dispatch(deleteUser(theUser._id)) // or 'id'. test this
     }
 }
 
